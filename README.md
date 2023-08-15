b9dc0dac3ca7725094a2f61c813f2804eb748bc8
YelloMush <somplier2245@gmail.com>
2023-08-15 18:13:15 +0530

    Added Feature to Delete all data

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Simple python-based program to create a daily Schedule Planner
 
 ## Instruction 
 
-Install Python file to run
+Run in CMD
--- schedule_oop.py
+++ schedule_oop.py
@@ -1,7 +1,5 @@
-import pandas as pd 
 import pickle
 from prettytable import PrettyTable
-
 
 class SchedulePlanner:
     def __init__(self):
@@ -12,11 +10,11 @@ class SchedulePlanner:
 
     def view_schedule(self):
         if not self.activities:
-            print("No activities in the schedule")
+            print("No activities in the schedule.")
             return
-        
+
         table = PrettyTable()
-        table.field_names = ['Acitvity', 'Time']
+        table.field_names = ["Activity", "Time"]
 
         for activity, time in self.activities:
             table.add_row([activity, time])
@@ -24,81 +22,90 @@ class SchedulePlanner:
         print(table)
 
     def delete_activity(self, activity):
+        found = False
         for item in self.activities:
             if item[0] == activity:
                 self.activities.remove(item)
-            print(f"Activity {activity} succesfully deleted!")
-        print(f"Activity {activity} not found!")
-        return
-    
+                print(f"Activity '{activity}' deleted from the schedule.")
+                found = True
+                break
+
+        if not found:
+            print(f"Activity '{activity}' not found in the schedule.")
+
+        self.save_to_file()  # Save updated data to the file
+
+    def delete_all_activities(self):
+        self.activities = [] #creates empty list which deletes existing data
+        self.save_to_file() #deletes existing data in the file
+
     def save_to_file(self):
         with open('schedule_planner.pkl', 'wb') as file:
             pickle.dump(self.activities, file)
 
-    def load_to_file(self):
+    def load_from_file(self):
         try:
             with open('schedule_planner.pkl', 'rb') as file:
-                pickle.load(file)
-                print("Schedule Data loaded from file")
+                self.activities = pickle.load(file)
+                print("Schedule data loaded from file.")
         except FileNotFoundError:
-            print("No saved schedule data exist!")
+            print("No saved schedule data found.")
+
 def main():
     schedule_planner = SchedulePlanner()
-    schedule_planner.load_to_file()
 
-    
-#class ClassSchedule:
-#    def __init__(self, activity, timing):
-#        self.activity = activity
-#        self.timing = timing 
-#    
-#    def display_schdle(self):
-#        print(f'Acitvity: {self.activity}, Time: {self.timing}')
+    # Load existing schedule data from the file (if available)
+    schedule_planner.load_from_file()
 
-while True:
-    ques = input("--- Select one: --- \
-                 \n(A) Create a new activity \
-                 \n(B) View Existing Schedule \
-                 \n(C) Delete Existing Data \
-                 \n(Q) Quit \
-                 \nYour Choice?: ")
-    
-#    if ques.lower() == 'q':
-#
-#        data_to_save = {
-#            'activity': ACTIVITY,
-#            'time': TIME
-#        }
-#        with open('schedule_planner.pkl', 'wb') as file:
-#            pickle.dump(data_to_save, file)
-#
-#        break
+    while True:
+        print("--- Schedule Planner ---")
+        print("[A] Add Activity")
+        print("[B] View Schedule")
+        print("[C] Delete Activity")
+        print("[Q] Quit")
 
-    if ques.lower() == 'a':
-        activity = input("Enter the Activity Name: ")
-        time = input("Enter the Time of Activity: ")
+        choice = input("Enter your choice: ")
 
-        SchedulePlanner.add_activity(activity, time)
-        print("Activity added to the schedule")
-        SchedulePlanner.save_to_file()
+        if choice.lower() == 'a':
+            activity = input("Enter the Activity Name: ")
+            time = input("Enter the Time of Activity: ")
+            schedule_planner.add_activity(activity, time)
+            print("Activity added to the schedule.")
+            schedule_planner.save_to_file()  # Save updated data to the file
 
-    elif ques.lower() == 'b':
-        SchedulePlanner.view_schedule()
+        elif choice.lower() == 'b':
+            schedule_planner.view_schedule()
 
-    elif ques.lower() == 'c':
+        elif choice.lower() == 'c':
+            ques = input("[1] Delete All Activities\
+                     \n[2] Delete Specific Activity\
+                     \nYour Choice? : ")
+            
+            if  ques == '2':
+                activity_to_delete = input("Enter the Activity Name to delete: ")
+                schedule_planner.delete_activity(activity_to_delete)
 
-        acitvity_to_delete = input("Enter Activity Name to delete: ")
-        SchedulePlanner.delete_activity(acitvity_to_delete)
+            elif ques == '1':
+                if input("Are you sure you want delete all activities? [y/n] :") != 'n':
+                    schedule_planner.delete_all_activities()
+                    print("All activties deleted!")
+                else:
+                    break
 
-    elif ques.lower() == 'q':
-        print('See you Later!')
-        break
+            else:
+                return "Invalid Choice!" 
 
-    else:
-        print("Invalid Choice. Please select 'A', 'B' or 'Q'")
-    
-    if input('Do You Want To Continue? [y/n]') != 'y':
-        SchedulePlanner.save_to_file()  
+        elif choice.lower() == 'q':
+            print("See you Later!")
+            break
+
+        else:
+            print("Invalid choice. Please select a valid option.")
+        
+        if input("Do you want to continue? [y/n] : ") != 'y':
+            schedule_planner.save_to_file()
+            break
+        
 
 if __name__ == "__main__":
     main()
